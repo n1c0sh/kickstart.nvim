c8151e6fce4163531303ca1626e6ff6e96d3fb1d
n1c0sh <n1c0sh@users.noreply.github.com>
2025-02-21 11:22:04 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -658,7 +658,9 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {},
+        java_language_server = {},
+        nginx_language_server = {},  
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
